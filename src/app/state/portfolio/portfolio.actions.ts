fcea849b033c7c0cc46c6869eb5dcdd36afb6d9a
divyanshu <divyanshurajput.dev@gmail.com>
2026-02-26 21:34:41 +0530

    add portfolio store actions

---
+++ src/app/state/portfolio/portfolio.actions.ts
@@ -0,0 +1,11 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+
+import type { Holding } from '../../shared/models/holding.model';
+
+export const PortfolioActions = createActionGroup({
+  source: 'Portfolio',
+  events: {
+    'Load Snapshot': emptyProps(),
+    'Snapshot Loaded': props<{ cash: number; holdings: Holding[] }>(),
+  },
+});
