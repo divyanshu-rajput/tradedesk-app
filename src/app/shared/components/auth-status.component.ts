aeb46cec9e41e2125fce1aa25151a6601f7e60e5
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-04 22:58:44 +0530

    start auth status component

---
+++ src/app/shared/components/auth-status.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-status',
+  standalone: true,
+  templateUrl: './auth-status.component.html',
+  styleUrl: './auth-status.component.scss',
+})
+export class AuthStatusComponent {}
