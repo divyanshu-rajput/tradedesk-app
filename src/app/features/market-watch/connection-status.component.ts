522039e2f55b1aaed9ee8a8a40c8af6a5bc49648
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-08 08:05:58 +0530

    start connection status component

---
+++ src/app/features/market-watch/connection-status.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-status',
+  standalone: true,
+  templateUrl: './connection-status.component.html',
+  styleUrl: './connection-status.component.scss',
+})
+export class ConnectionStatusComponent {}
