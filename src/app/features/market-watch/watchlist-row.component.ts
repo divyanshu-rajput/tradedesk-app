78dd5ff1483721f3ef1d6ac6b0b59493913e089d
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-09 20:51:38 +0530

    start watchlist row component

---
+++ src/app/features/market-watch/watchlist-row.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watchlist-row',
+  standalone: true,
+  templateUrl: './watchlist-row.component.html',
+  styleUrl: './watchlist-row.component.scss',
+})
+export class WatchlistRowComponent {}
