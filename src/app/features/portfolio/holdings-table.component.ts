83c4f473ec302178b291e56f62e782f0eb47e662
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-10 19:17:07 +0530

    feat: add holdings table logic

---
+++ src/app/features/portfolio/holdings-table.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { formatCurrency, formatHoldingQty, formatPct } from '../../shared/utils/number-format';
+import { formatSymbolLabel } from '../../shared/utils/symbol-format';
+import { selectHoldingsWithPnl } from '../../state/portfolio/portfolio.selectors';
+
+@Component({
+  selector: 'app-holdings-table',
+  templateUrl: './holdings-table.component.html',
+  styleUrl: './holdings-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HoldingsTableComponent {
+  private readonly store = inject(Store);
+
+  readonly holdings = this.store.selectSignal(selectHoldingsWithPnl);
+  readonly formatSymbol = formatSymbolLabel;
+  readonly formatCurrency = formatCurrency;
+  readonly formatHoldingQty = formatHoldingQty;
+  readonly formatPct = formatPct;
+}
