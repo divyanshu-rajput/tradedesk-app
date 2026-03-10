48abfd1fc5c9274b02b7374373d00ab38e1910bc
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-10 23:27:38 +0530

    start order placement component

---
+++ src/app/features/order-placement/order-placement.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-placement',
+  standalone: true,
+  templateUrl: './order-placement.component.html',
+  styleUrl: './order-placement.component.scss',
+})
+export default class OrderPlacementComponent {}
