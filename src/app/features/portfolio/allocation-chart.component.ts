cb3fa3a8c138e4352c3dd48f1ccc583469bb538e
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-13 02:07:22 +0530

    start allocation chart component

---
+++ src/app/features/portfolio/allocation-chart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allocation-chart',
+  standalone: true,
+  templateUrl: './allocation-chart.component.html',
+  styleUrl: './allocation-chart.component.scss',
+})
+export class AllocationChartComponent {}
