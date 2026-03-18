9d6fe885cbe6e72fafcab3129b2fcb194a396fd5
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-18 22:04:58 +0530

    start price line chart

---
+++ src/app/features/charts/price-line-chart.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-line-chart',
+  standalone: true,
+  templateUrl: './price-line-chart.component.html',
+  styleUrl: './price-line-chart.component.scss',
+})
+export class PriceLineChartComponent {}
