5167031b6cddcb754a78d11d3f02b9be9540ee55
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-28 08:25:42 +0530

    refactor: remove market connect from market watch

--- src/app/features/market-watch/market-watch.component.ts
+++ src/app/features/market-watch/market-watch.component.ts
@@ -1,8 +1,6 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { WATCHLIST_SYMBOLS } from '../../core/market-data/market.constants';
-import { MarketActions } from '../../state/market/market.actions';
 import { ConnectionStatusComponent } from './connection-status.component';
 import { WatchlistTableComponent } from './watchlist-table.component';
 
@@ -13,17 +11,6 @@ import { WatchlistTableComponent } from './watchlist-table.component';
   styleUrl: './market-watch.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class MarketWatchComponent implements OnInit {
-  private readonly store = inject(Store);
-  private readonly destroyRef = inject(DestroyRef);
-
+export default class MarketWatchComponent {
   readonly symbolCount = WATCHLIST_SYMBOLS.length;
-
-  ngOnInit(): void {
-    this.store.dispatch(MarketActions.connect());
-
-    this.destroyRef.onDestroy(() => {
-      this.store.dispatch(MarketActions.disconnect());
-    });
-  }
 }
