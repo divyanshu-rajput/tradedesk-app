c67fd9b473bcad3af5063b66da329c8e2b901f13
divyanshu <divyanshurajput.dev@gmail.com>
2026-03-28 18:57:32 +0530

    refactor: remove market connect from portfolio page

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { formatCurrency } from '../../shared/utils/number-format';
-import { MarketActions } from '../../state/market/market.actions';
 import {
   selectAllocation,
   selectCash,
@@ -19,21 +18,12 @@ import { HoldingsTableComponent } from './holdings-table.component';
   styleUrl: './portfolio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PortfolioComponent implements OnInit {
+export default class PortfolioComponent {
   private readonly store = inject(Store);
-  private readonly destroyRef = inject(DestroyRef);
 
   readonly cash = this.store.selectSignal(selectCash);
   readonly totalValue = this.store.selectSignal(selectTotalPortfolioValue);
   readonly totalPnl = this.store.selectSignal(selectTotalPnl);
   readonly allocation = this.store.selectSignal(selectAllocation);
   readonly formatCurrency = formatCurrency;
-
-  ngOnInit(): void {
-    this.store.dispatch(MarketActions.connect());
-
-    this.destroyRef.onDestroy(() => {
-      this.store.dispatch(MarketActions.disconnect());
-    });
-  }
 }
